32ac8dda373a76fafb6fa19ae306cd9515adbea7
giri1987 <p.girish8@gmail.com>
2017-11-20 16:55:45 +0530

    broadcast variable example

---
+++ broadcast.py
@@ -0,0 +1,15 @@
+#Broadcast Variable :
+#====================
+
+states=sqlContext.read.json("/dev/datalake/app/gcr/medt/input/us_states.json")
+brd_states=sc.broadcast(states.collect())
+brd_schema=sc.broadcast(states.schema)
+storesDF=sqlContext.read.json("/dev/datalake/app/gcr/medt/input/store_locations.json")
+
+#Creation of dataframe
+#---------------------
+
+df=sqlContext.createDataFrame(brd_states.value,brd_schema.value)
+f_rdd=storesDF.join(df,"state").groupBy("census_division").collect()
+
+f_rdd.show()
