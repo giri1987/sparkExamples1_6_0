e1c7341b819a794a3a19a8d6264f19a80ce95bc9
giri1987 <p.girish8@gmail.com>
2017-11-24 16:38:53 +0530

    wordCound using accumulator

---
+++ wordCountAccumuator.py
@@ -0,0 +1,4 @@
+acc=sc.accumulator(0)
+wiki=sc.textFile("/dev/datalake/app/gcr/medt/input/wiki.txt"
+wiki.flatMap(lambda x : x.split()).map(lambda x: acc.add(1)).collect()
+print("Total words : ",acc.value)
