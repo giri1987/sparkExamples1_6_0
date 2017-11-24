d6801c1057a1688fa98061da09de3d1c4043c2f1
giri1987 <p.girish8@gmail.com>
2017-11-24 15:00:34 +0530

    added ouput

--- broadcast.py
+++ broadcast.py
@@ -12,3 +12,21 @@ storesDF=sqlContext.read.json("/dev/datalake/app/gcr/medt/input/store_locations.
 df=sqlContext.createDataFrame(brd_states.value,brd_schema.value)
 f_rdd=storesDF.join(df,"state").groupBy("census_division").agg({"census_division":"count"})
 f_rdd.show()
+
+
+#Output
+
+#+------------------+--------------------+
+#|   census_division|count(census_region)|
+#+------------------+--------------------+
+#|   Middle Atlantic|                  45|
+#|East South Central|                  12|
+#|East North Central|                  43|
+#|    South Atlantic|                  74|
+#|           Pacific|                 174|
+#|          Mountain|                  48|
+#|West South Central|                  31|
+#|West North Central|                  21|
+#|                  |                   6|
+#|       New England|                  21|
+#+------------------+--------------------+
